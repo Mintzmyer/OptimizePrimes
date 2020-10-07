b7800efb3bc19e046e65f4851a54b9053e8247e6
Samantha Feinstein <Samantha.I.Feinstein@ci.eugene.or.us>
2020-10-07 11:55:20 -0700

    Added Powershell for Windows Compatibility
    Added a powershell version of the script
    Initial tests seem to be quite a bit faster
    than it's bash equivalent

---
+++ PrimeFinder/PrimeFinder.ps1
@@ -0,0 +1,74 @@
+#
+# This program finds primes!
+#     But not particularly well, or anything
+# 
+# Set this to run on start up, it syncs with git
+#     picks up where it left off, and periodically commits new findings
+#
+
+
+echo "Starting PrimeFinder"
+git fetch
+git pull
+
+$Primes = @()
+$RecentPrimes = @()
+$LastSave=2
+
+$PRIMESFILE="./CurrentPrimes.txt"
+$STARTFILE="./LastSave.txt"
+
+# Get list of found primes
+if (Test-Path "$PRIMESFILE") {
+  $Primes= Get-Content $PRIMESFILE
+}
+
+echo ("Loaded " + $Primes.length + " primes")
+
+# Get bookmarked number to start at
+if (Test-Path "$STARTFILE") {
+  [int]$LastSave= Get-Content $STARTFILE
+}
+
+[int]$Current=$LastSave
+echo ("Starting at: " + $Current)
+
+# Begin the hunt!!!
+while (1) {
+  $isPrime=1
+  foreach ($prime in $Primes) {
+    #echo ("Current: " + $Current + " | i: " + $prime)
+    if (-Not ( $Current % $prime )) {
+      $isPrime=0;
+      continue
+    }
+  }
+
+  # If we found a prime, add it to the file
+  if ($isPrime) {
+    echo ("Found " + $Current)
+    $Current.ToString() | Out-File "$PRIMESFILE" -Append -Encoding "UTF8"
+    $Current.ToString() | Out-File "$STARTFILE" -Encoding "UTF8"
+    $Primes+=$Current
+    $RecentPrimes+=$Current
+  }
+
+  # Well, maybe bundle it in batches of 5 before we push to the repo
+  if ( $RecentPrimes.length > 5 ) {
+    #git commit -am "Found more primes: ${RecentPrimes[*]}"
+    #git push
+    $RecentPrimes=@()
+    $LastSave=$Current
+  }
+
+  # Add a save point every 1000 checked numbers
+  if ( $Current - $LastSave > 1000 ) {
+    $Current.ToString() | Out-File "$STARTFILE" -Encoding "UTF8"
+
+    #git commit -am "At $Current, nothing in the last 1000"
+    #git push
+    $LastSave=$Current
+  }
+
+  $Current++
+}
